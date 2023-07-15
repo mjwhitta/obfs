a31db1ba37f8fd906c35800d646e0a8c4f386802
Miles Whittaker <mj@whitta.dev>
2023-07-15 18:08:58 -0400

    Updated deps

--- cmd/obfstr/cli.go
+++ cmd/obfstr/cli.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"strings"
 
 	"github.com/mjwhitta/cli"
 	hl "github.com/mjwhitta/hilighter"
@@ -33,23 +32,17 @@ func init() {
 	cli.Authors = []string{"Miles Whittaker <mj@whitta.dev>"}
 	cli.Banner = hl.Sprintf("%s [OPTIONS] <str>", os.Args[0])
 	cli.BugEmail = "obfs.bugs@whitta.dev"
-	cli.ExitStatus = strings.Join(
-		[]string{
-			"Normally the exit status is 0. In the event of an error",
-			"the exit status will be one of the below:\n\n",
-			hl.Sprintf("%d: Invalid option\n", InvalidOption),
-			hl.Sprintf("%d: Missing option\n", MissingOption),
-			hl.Sprintf("%d: Invalid argument\n", InvalidArgument),
-			hl.Sprintf("%d: Missing argument\n", MissingArgument),
-			hl.Sprintf("%d: Extra argument\n", ExtraArgument),
-			hl.Sprintf("%d: Exception", Exception),
-		},
-		" ",
+	cli.ExitStatus(
+		"Normally the exit status is 0. In the event of an error the",
+		"exit status will be one of the below:\n\n",
+		hl.Sprintf("%d: Invalid option\n", InvalidOption),
+		hl.Sprintf("%d: Missing option\n", MissingOption),
+		hl.Sprintf("%d: Invalid argument\n", InvalidArgument),
+		hl.Sprintf("%d: Missing argument\n", MissingArgument),
+		hl.Sprintf("%d: Extra argument\n", ExtraArgument),
+		hl.Sprintf("%d: Exception", Exception),
 	)
-	cli.Info = strings.Join(
-		[]string{"Generate code for obfuscated string."},
-		" ",
-	)
+	cli.Info("Generate code for obfuscated string.")
 	cli.Title = "Obfs"
 
 	// Parse cli flags
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/mjwhitta/obfs
 go 1.19
 
 require (
-	github.com/mjwhitta/cli v1.10.1
-	github.com/mjwhitta/errors v1.0.1
-	github.com/mjwhitta/hilighter v1.11.2
-	github.com/mjwhitta/log v1.6.3
+	github.com/mjwhitta/cli v1.11.0
+	github.com/mjwhitta/errors v1.0.2
+	github.com/mjwhitta/hilighter v1.11.3
+	github.com/mjwhitta/log v1.6.4
 )
