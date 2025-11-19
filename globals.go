7bac28a0967869f2133609615d13bb10732b708b
Miles Whittaker <mj@whitta.dev>
2025-11-18 22:51:35 -0500

    Updated deps

--- globals.go
+++ globals.go
@@ -1,7 +1,7 @@
 package obfs
 
 // Version is the package version.
-const Version string = "1.3.0"
+const Version string = "1.3.1"
 
 // MaxInc will determine the maximum increment size when obfuscating
 // data.
